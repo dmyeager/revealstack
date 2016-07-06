3899adf0f0a3e3901a01a1b59fa1bfb8bdd31814
Dave Yeager <david.m.yeager@gmail.com>
2016-07-05 22:25:07 -0400

    adding some extra api stuff, chart skeleton page

--- app.js
+++ app.js
@@ -3,6 +3,41 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
+var pg = require('pg');
+var connString = process.env.DATABASE_URL || 'postgres://localhost:5432/davidyeager';
+
+app.get('/api/byDayOfWeek', function (req, res) {
+	var client = new pg.Client(connString);
+	var results = [];
+	client.connect();
+	var query = client.query("select date_part('dow', start_dt), avg(energy) from energy_data group by date_part('dow', start_dt) order by 1");
+	query.on('row', function(row) {
+		results.push(row);
+	});
+	query.on('end', function() {
+		client.end();
+		res.json(results);
+	});
+});
+
+app.get('/api/byHour', function (req, res) {
+	var client = new pg.Client(connString);
+	var results = [];
+	client.connect();
+	var query = client.query("select date_part('hour', start_dt), avg(energy) from energy_data group by date_part('hour', start_dt) order by 1");
+	query.on('row', function(row) {
+		results.push(row);
+	});
+	query.on('end', function() {
+		client.end();
+		res.json(results);
+	});
+});
+
+app.get('/chart', function (req, res) {
+	res.sendfile(__dirname, '/chart.html');
+});
+
 app.use('/', express.static(__dirname));
 app.get('/', function (req, res) {
 	res.redirect('/reveal.js/openee.html')
